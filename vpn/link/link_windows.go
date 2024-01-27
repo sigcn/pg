85d20b3bf148b0e8f32abdf23cdcadbe07775666
rkonfj <rkonfj@gmail.com>
2024-01-27 11:13:37 +0800

    vpn/link: fix link ipv6 setup failed on windows

--- vpn/link/link_windows.go
+++ vpn/link/link_windows.go
@@ -19,10 +19,10 @@ func SetupLink(device tun.Device, cidr string) error {
 	if err != nil {
 		return err
 	}
-	ctx := "ipv4"
-	if ip.To4() == nil {
-		ctx = "ipv6"
+	if ip.To4() == nil { // ipv6
+		return exec.Command("netsh", "interface", "ipv6", "add", "address", ifName, cidr).Run()
 	}
+	// ipv4
 	addrMask := fmt.Sprintf("%d.%d.%d.%d", ipnet.Mask[0], ipnet.Mask[1], ipnet.Mask[2], ipnet.Mask[3])
-	return exec.Command("netsh", "interface", ctx, "set", "address", ifName, "static", ip.String(), addrMask).Run()
+	return exec.Command("netsh", "interface", "ipv4", "set", "address", ifName, "static", ip.String(), addrMask).Run()
 }
