e65e3f20667ec86b2e61e8e4c28566f135377dad
rkonfj <rkonfj@gmail.com>
2024-11-05 20:54:52 +0800

    cmd/pgmap: use flag instead of cobra

--- cmd/pgmap/main.go
+++ cmd/pgmap/main.go
@@ -3,62 +3,100 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
+	"runtime/debug"
+	"strings"
 	"syscall"
 
 	"github.com/sigcn/pg/peermap"
-	"github.com/spf13/cobra"
 )
 
 var (
-	Version = "unknown"
-	Commit  = "unknown"
+	Version = "dev"
 )
 
-func main() {
-	serveCmd := &cobra.Command{
-		Use:          "pgmap",
-		Version:      fmt.Sprintf("%s, commit %s", Version, Commit),
-		Short:        "Run a peermap server daemon",
-		SilenceUsage: true,
-		PreRunE: func(cmd *cobra.Command, args []string) error {
-			verbose, err := cmd.Flags().GetInt("verbose")
-			if err != nil {
-				return err
-			}
-			slog.SetLogLoggerLevel(slog.Level(verbose))
-			return nil
-		},
-		Args: cobra.NoArgs,
-		RunE: run,
-	}
-	serveCmd.Flags().StringP("config", "c", "config.yaml", "config file")
-	serveCmd.Flags().StringP("listen", "l", "127.0.0.1:9987", "listen http address")
-	serveCmd.Flags().String("secret-key", "", "key to generate network secret (defaut generate a random one)")
-	serveCmd.Flags().StringSlice("stun", []string{}, "stun server for peers NAT traversal (leave blank to disable NAT traversal)")
-	serveCmd.Flags().String("pubnet", "", "public network (leave blank to disable public network)")
-	serveCmd.Flags().IntP("verbose", "V", 0, "logger verbosity level")
+type stuns []string
 
-	serveCmd.Execute()
+func (s *stuns) String() string {
+	return strings.Join(*s, ",")
 }
 
-func run(cmd *cobra.Command, args []string) error {
-	cfg1, err := commandlineConfig(cmd)
-	if err != nil {
-		return err
-	}
+func (s *stuns) Set(stun string) error {
+	*s = append(*s, stun)
+	return nil
+}
 
-	configFile, err := cmd.Flags().GetString("config")
-	if err != nil {
-		return err
-	}
+func main() {
+	var (
+		configPath string
+		logLevel   int
+		stuns      stuns
+	)
+	flag.StringVar(&configPath, "config", "config.yml", "")
+	flag.StringVar(&configPath, "c", "config.yml", "config file")
+	flag.IntVar(&logLevel, "loglevel", int(slog.LevelInfo), "log level (default is 0:info)")
+	flag.Var(&stuns, "stun", "stun server for peers NAT traversal (leave blank to disable NAT traversal)")
 
-	cfg, _ := peermap.ReadConfig(configFile)
-	cfg.Overwrite(cfg1)
+	commandConfig := peermap.Config{STUNs: stuns}
+	flag.StringVar(&commandConfig.Listen, "listen", "127.0.0.1:9987", "")
+	flag.StringVar(&commandConfig.Listen, "l", "127.0.0.1:9987", "listen http address")
+	flag.StringVar(&commandConfig.SecretKey, "secret-key", "", "key to generate network secret (defaut generate a random one)")
+	flag.StringVar(&commandConfig.PublicNetwork, "pubnet", "", "public network (leave blank to disable public network)")
+	flag.BoolFunc("version", "", printVersion)
+	flag.BoolFunc("v", "print version", printVersion)
+
+	flag.Usage = usage
+	flag.Parse()
+
+	slog.SetLogLoggerLevel(slog.Level(logLevel))
+	if err := run(commandConfig, configPath); err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+func printVersion(string) error {
+	var buildTime, commit string
+	info, ok := debug.ReadBuildInfo()
+	if ok {
+		for _, kv := range info.Settings {
+			if kv.Key == "vcs.revision" {
+				commit = kv.Value
+				continue
+			}
+			if kv.Key == "vcs.time" {
+				buildTime = kv.Value
+			}
+		}
+	}
+	fmt.Println(info.GoVersion)
+	fmt.Printf("version %s\n", Version)
+	fmt.Printf("build_time %s\n", buildTime)
+	fmt.Printf("commit %s\n", commit)
+	os.Exit(0)
+	return nil
+}
+
+func usage() {
+	fmt.Printf("Run a peermap server daemon\n\n")
+	fmt.Printf("Usage of %s:\n", os.Args[0])
+	fmt.Printf("  -c, --config string\n\t%s (default is \"%s\")\n", flag.Lookup("c").Usage, flag.Lookup("c").DefValue)
+	fmt.Printf("  -l, --listen string\n\t%s (default is \"%s\")\n", flag.Lookup("l").Usage, flag.Lookup("l").DefValue)
+	fmt.Printf("  --loglevel int\n\t%s\n", flag.Lookup("loglevel").Usage)
+	fmt.Printf("  --pubnet string\n\t%s\n", flag.Lookup("pubnet").Usage)
+	fmt.Printf("  --secret-key string\n\t%s\n", flag.Lookup("secret-key").Usage)
+	fmt.Printf("  --stun []string\n\t%s\n", flag.Lookup("stun").Usage)
+	fmt.Printf("  -v, --version\n\t%s\n", flag.Lookup("v").Usage)
+}
+
+func run(commandConfig peermap.Config, configPath string) error {
+	cfg, _ := peermap.ReadConfig(configPath)
+	cfg.Overwrite(commandConfig)
 
 	srv, err := peermap.New(cfg)
 	if err != nil {
@@ -70,21 +108,4 @@ func run(cmd *cobra.Command, args []string) error {
 		return err
 	}
 	return nil
-}
-
-func commandlineConfig(cmd *cobra.Command) (opts peermap.Config, err error) {
-	opts.Listen, err = cmd.Flags().GetString("listen")
-	if err != nil {
-		return
-	}
-	opts.SecretKey, err = cmd.Flags().GetString("secret-key")
-	if err != nil {
-		return
-	}
-	opts.PublicNetwork, err = cmd.Flags().GetString("pubnet")
-	if err != nil {
-		return
-	}
-	opts.STUNs, err = cmd.Flags().GetStringSlice("stun")
-	return
 }
