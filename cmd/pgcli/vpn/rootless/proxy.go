d35a2276e2a9c49c376c40bb795d038fd02f1972
rkonfj <rkonfj@gmail.com>
2024-12-28 22:30:39 +0800

    pgcli/vpn: add http proxy protocol

--- cmd/pgcli/vpn/rootless/proxy.go
+++ cmd/pgcli/vpn/rootless/proxy.go
@@ -1,10 +1,15 @@
 package rootless
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"log/slog"
 	"net"
+	"net/http"
+	"strings"
 	"sync"
 	"time"
 
@@ -12,6 +17,24 @@ import (
 	"github.com/sigcn/pg/socks5"
 	"github.com/sigcn/pg/vpn/nic/gvisor"
 )
+
+type peekConn struct {
+	net.Conn
+	peekBytes []byte
+}
+
+func (c *peekConn) Read(b []byte) (n int, err error) {
+	if c.peekBytes != nil {
+		n = copy(b, c.peekBytes)
+		if n < len(c.peekBytes) {
+			c.peekBytes = c.peekBytes[n:]
+		} else {
+			c.peekBytes = nil
+		}
+		return
+	}
+	return c.Conn.Read(b)
+}
 
 type ProxyConfig struct {
 	Listen string
@@ -42,7 +65,7 @@ func (s *ProxyServer) Start(ctx context.Context, wg *sync.WaitGroup) error {
 		udpPacketConn.Close()
 	}()
 	s.udpListener = &N.UDPListener{PacketConn: udpPacketConn}
-	slog.Info("[Proxy] Server started", "listen", fmt.Sprintf("tcp+udp://%s", tcpListener.Addr().String()), "protocols", "socks5")
+	slog.Info("[Proxy] Server started", "listen", fmt.Sprintf("tcp+udp://%s", tcpListener.Addr().String()), "protocols", "socks5,http")
 	go s.run(tcpListener)
 	return nil
 }
@@ -53,50 +76,117 @@ func (s *ProxyServer) run(tcp net.Listener) {
 		if err != nil {
 			return
 		}
-		addr, cmd, err := socks5.ServerHandshake(c, nil)
-		if err != nil {
-			slog.Error("[Proxy] SOCKS5 handshake", "err", err)
+
+		b := make([]byte, 1)
+		if _, err = io.ReadFull(c, b); err != nil {
 			continue
 		}
-		if cmd == socks5.CmdConnect {
-			if err := s.proxyTCP(c, addr); err != nil {
-				slog.Error("[Proxy] SOCKS5 tcp", "err", err)
-			}
-			continue
-		}
-		if cmd == socks5.CmdUDPAssociate {
-			go func() {
-				if err := s.proxyUDP(addr); err != nil {
-					slog.Error("[Proxy] SOCKS5 udp", "err", err)
+
+		conn := &peekConn{Conn: c, peekBytes: b}
+		go func() {
+			if b[0] == 5 {
+				if err := s.serveSOCKS5(conn); err != nil {
+					slog.Error("[Proxy] Serve socks5", "err", err)
 				}
-			}()
-			continue
-		}
+				return
+			}
+			if err := s.serveHTTP(conn); err != nil {
+				slog.Error("[Proxy] Serve http", "err", err)
+			}
+		}()
 	}
 }
 
-func (s *ProxyServer) proxyTCP(rw net.Conn, addr socks5.Addr) error {
-	c, err := s.GvisorCard.DialContext(context.TODO(), "tcp", addr.String())
+func (s *ProxyServer) serveSOCKS5(c net.Conn) error {
+	defer c.Close()
+	addr, cmd, err := socks5.ServerHandshake(c, nil)
 	if err != nil {
-		rw.Close()
-		return err
+		return fmt.Errorf("socks5 handshake: %w", err)
 	}
-	go relay(rw, c)
+	if cmd == socks5.CmdConnect {
+		if err := s.proxyTCP(c, addr.String()); err != nil {
+			return fmt.Errorf("socks5 proxy tcp: %w", err)
+		}
+		return nil
+	}
+	if cmd == socks5.CmdUDPAssociate {
+		if err := s.proxyUDP(addr.String()); err != nil {
+			return fmt.Errorf("socks5 proxy udp: %w", err)
+		}
+		return nil
+	}
 	return nil
 }
 
-func (s *ProxyServer) proxyUDP(addr socks5.Addr) error {
+func (s *ProxyServer) serveHTTP(c net.Conn) error {
+	defer c.Close()
+	r, err := http.ReadRequest(bufio.NewReader(c))
+	if err != nil {
+		return fmt.Errorf("http parse request: %w", err)
+	}
+
+	if _, _, err := net.SplitHostPort(r.Host); err != nil {
+		r.Host = fmt.Sprintf("%s:80", r.Host)
+	}
+
+	if r.Method == http.MethodConnect {
+		_, err = c.Write([]byte("HTTP/1.1 200 Connection established\r\n\r\n"))
+		if err != nil {
+			return err
+		}
+		if err = s.proxyTCP(c, r.Host); err != nil {
+			s.responseError(c, err)
+			return err
+		}
+		return nil
+	}
+
+	r.Header.Del("Proxy-Connection")
+	b := &bytes.Buffer{}
+	r.Write(b)
+
+	if err = s.proxyTCP(&peekConn{Conn: c, peekBytes: b.Bytes()}, r.Host); err != nil {
+		s.responseError(c, err)
+		return err
+	}
+	return nil
+}
+
+func (s *ProxyServer) proxyTCP(rw net.Conn, addr string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	c, err := s.GvisorCard.DialContext(ctx, "tcp", addr)
+	if err != nil {
+		return err
+	}
+	relay(rw, c)
+	return nil
+}
+
+func (s *ProxyServer) proxyUDP(addr string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	c, err := s.udpListener.AcceptContext(ctx)
 	if err != nil {
 		return err
 	}
-	c1, err := s.GvisorCard.DialContext(context.TODO(), "udp", addr.String())
+	c1, err := s.GvisorCard.DialContext(context.TODO(), "udp", addr)
 	if err != nil {
 		c.Close()
 		return err
 	}
-	go relay(c, c1)
+	relay(c, c1)
 	return nil
+}
+
+func (s *ProxyServer) responseError(w io.Writer, err error) {
+	r := &http.Response{
+		Status:     "500 InternalServerError",
+		StatusCode: http.StatusInternalServerError,
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Proto:      "HTTP/1.1",
+		Body:       io.NopCloser(strings.NewReader(err.Error() + "\n")),
+	}
+	r.Write(w)
 }
